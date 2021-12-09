c21778a6b161d43e7b7063157df0a308526f9813
Dario Herrera <dario.herrera@unosquare.com>
2021-12-09 17:59:14 -0600

    Product edit component updated

--- inventario/src/app/content/warehouse/products/components/products-edit/products-edit.component.ts
+++ inventario/src/app/content/warehouse/products/components/products-edit/products-edit.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Provider } from 'src/app/content/purchases/providers/interfaces/provider';
+import { ProvidersService } from 'src/app/content/purchases/providers/services/providers.service';
+import { Brand } from '../../../brands/interfaces/brand';
+import { BrandsService } from '../../../brands/services/brands.service';
+import { Category } from '../../../categories/interfaces/category';
+import { CategoriesService } from '../../../categories/services/categories.service';
+import { Presentation } from '../../../presentations/interfaces/presentation';
+import { PresentationsService } from '../../../presentations/services/presentations.service';
 
 @Component({
   selector: 'app-products-edit',
@@ -9,9 +17,46 @@ export class ProductsEditComponent implements OnInit {
 
   public title: string = 'Editar Producto';
 
-  constructor() { }
+  public brands: Brand[] = [];
+  public providers: Provider[] = [];
+  public categories: Category[] = [];
+  public presentations: Presentation[] = [];
+
+  constructor(
+    private readonly brandsService: BrandsService,
+    private readonly providersService: ProvidersService,
+    private readonly categoriesService: CategoriesService,
+    private readonly presentationsService: PresentationsService) { }
 
   ngOnInit(): void {
+    this.getBrands();
+    this.getProviders();
+    this.getCategories();
+    this.getPresentations();
+  }
+
+  private getBrands(): void {
+    this.brandsService.getBrands().subscribe((response): void => {
+      this.brands = response;
+    });
+  }
+
+  private getProviders(): void {
+    this.providersService.getProviders().subscribe((response): void => {
+      this.providers = response;
+    });
+  }
+
+  private getCategories(): void {
+    this.categoriesService.getCategories().subscribe((response): void => {
+      this.categories = response
+    });
+  }
+
+  private getPresentations(): void {
+    this.presentationsService.getPresentations().subscribe((response: Presentation[]): void => {
+      this.presentations = response;
+    });
   }
 
 }
